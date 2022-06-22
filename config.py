cbdc84388f033e1a205c6b480a3522111812efbe
rida7712 <107874592+rida7712@users.noreply.github.com>
2022-06-22 18:28:34 +0300

    Update config.py

--- config.py
+++ config.py
@@ -7,13 +7,13 @@ from pytgcalls import PyTgCalls
 if os.path.exists(".env"):
     load_dotenv(".env")
 
-API_ID = int(os.getenv("API_ID"))
-API_HASH = os.getenv("API_HASH")
-SESSION = os.getenv("SESSION")
-OWNER_NAME = os.getenv("OWNER_NAME")
-CHANNEL = os.getenv("CHANNEL")
-PHOTO_CH = os.getenv("PHOTO_CH")
-HNDLR = os.getenv("HNDLR", "$")
+API_ID = int(os.getenv("16727482"))
+API_HASH = os.getenv("55edb5638b3ce1b0dbeb00cc210525f2")
+SESSION = os.getenv("AQBV7ePaQ-1KsMyt3nQ4B3r_3OynIIbLh2435b9EVHIr6mlOrBkCj9W3vWzoWst6Qq3OBmrd7lWk2qcEvDdeBBy2hFA2xq6AH-jcdE0o_kAzGzf3mZ6HraLD3h6JQYeZBeJIz8Vlp4xkh5qSpO-Mt4DqYLyYSEY3oJhkW2Wzp3wm2lDsH4OwA7_Ym2kzBLXNh2Spmg1mEoCclcsEa93MTX70lm_6RTv5OoaONtkx_mt_5Tyd95Sq5tp_92lOWgNkrEJ9BF5ZB4xu2IPjwlUQ6iRn-TXB9XShjzVWBrIpodM6vY3Cop6UQQ-eujj2CPvAlelfToTnlIxEDDG-UeHUfYkJAAAAATmGO98A")
+OWNER_NAME = os.getenv("ࢪﺿـاۅي اެبن الـدورة")
+CHANNEL = os.getenv("x02x2")
+PHOTO_CH = os.getenv("https://l.top4top.io/p_2363dcjiw1.jpg")
+HNDLR = os.getenv("HNDLR", "#")
 SUDO_USERS = list(map(int, os.getenv("SUDO_USERS").split()))
 
 
